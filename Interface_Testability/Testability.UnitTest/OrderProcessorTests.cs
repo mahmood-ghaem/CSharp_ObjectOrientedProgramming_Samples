2b3a082f1f6c0af07c9d32e5081bfacb0e96d0a7
Sayed Mahmood Ghaemmaghami <63294201+mahmood-ghaem@users.noreply.github.com>
2020-10-27 13:04:53 +0100

    interface Testability
    A simple project of order and shipment with unit test

---
+++ Interface_Testability/Testability.UnitTest/OrderProcessorTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testability.UnitTest
+{
+    [TestClass]
+    public class OrderProcessorTests
+    {
+        // MethodName_Condition_Expectation
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Process_OrderIsAlredyShipped_ThrowsAnException()
+        {
+            var orderProcessor = new OrderProcessor(new FackeShippingCalculator());
+            var order = new Order
+            {
+                Shipment = new Shipment()
+            };
+            orderProcessor.Process(order);
+        }
+        
+        [TestMethod]
+        public void Process_OrdeIsNotShipped_ShoudSetTheShipmenPropertyOfTheOrder()
+        {
+            var orderProcessor = new OrderProcessor(new FackeShippingCalculator());
+            var order = new Order();
+
+            orderProcessor.Process(order);
+
+            Assert.IsTrue(order.IsShipped);
+            Assert.AreEqual(1, order.Shipment.Cost);
+            Assert.AreEqual(DateTime.Today.AddDays(1),order.Shipment.ShippingDate);
+        }
+    }
+
+    public class FackeShippingCalculator : IShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            return 1;
+        }
+    }
+}
---
+++ Interface_Testability/Testability.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testability.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testability.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29a567ec-ee54-4134-bc36-70489d75f6f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
